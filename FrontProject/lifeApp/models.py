1ca6f7d97b342407148896b9d65cbfb7be99678c
koushikMuthakana <kmuthakana@gmail>
2019-02-24 21:07:01 +0530

    Added Models

--- FrontProject/lifeApp/models.py
+++ FrontProject/lifeApp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+    topic_name=models.CharField(max_length=50,unique=True)
+
+    def __str__(self):
+        return self.topic_name
+
+        
+class WebPage(models.Model):
+    web_name=models.CharField(max_length=40,unique=True)
+    topic=models.ForeignKey(Topic,on_delete=models.CASCADE)
+    url=models.URLField(unique=True)
+
+    def __str__(self):
+        return self.web_name + str(self.url)
+    
+class AccessRecord(models.Model):
+    name=models.ForeignKey(WebPage,on_delete=models.CASCADE)
+    date=models.DateField()
+
+    def __str__(self):
+        return str(self.date)
+
+
+
